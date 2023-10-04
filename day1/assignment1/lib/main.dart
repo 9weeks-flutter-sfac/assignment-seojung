832e61c3c1c1dad7362dd0cde79d5a8ee7ed71b8
sjchoi12 <seojungc0612@gmail.com>
2023-10-04 11:40:22 +0900

    1일차 과제

--- day1/assignment1/lib/main.dart
+++ day1/assignment1/lib/main.dart
@@ -1,6 +1,6 @@
+
 import 'package:flutter/material.dart';
 
-<<<<<<< HEAD
 void main() {
   runApp(const MyApp());
 }
@@ -69,24 +69,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-=======
-// void main() {
-//   runApp(const MyApp());
-// }
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-//       ),
-//       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-//     );
-//   }
-// }
->>>>>>> 4c9cac7be2bfe163271d83a87df29c634e75db02
+
